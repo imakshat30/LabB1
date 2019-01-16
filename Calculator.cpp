c44d7367c41079a21bebcf2983c1745059931388
imakshat30 <46739273+imakshat30@users.noreply.github.com>
2019-01-16 12:43:09 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class calc
+{
+	int add(int, int);
+	int divide(int, int);
+	
+	void subtract(int, int);
+	void multiply(int, int);
+
+		public:
+	int op1, op2
+}
